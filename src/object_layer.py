a9c766ebd3d762306bc3c833d172b694d1e3fad2
evrardco <colin.evrard@student.uclouvain.be>
2021-03-20 17:48:16 +0100

    added object_layer parsing

---
+++ src/object_layer.py
@@ -0,0 +1,13 @@
+import json
+from os.path import join
+class ObjectParser:
+    def __init__(self, name):
+        self.root = None
+        with open(join("assets", "maps", f"{name}.json"), "r") as f:
+            self.root = json.load(f)
+        for l in self.root["layers"]:
+            if l["name"] == "path_finding":
+                self.path_finding = sorted(l["objects"], key=lambda x: x["name"])
+            if l["name"] == "tower_spots":
+                self.tower_spots = l["objects"]
+
