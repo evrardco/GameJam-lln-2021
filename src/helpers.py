bdf3ffa096a33aebfbea339734bba4941e5d24d8
evrardco <colin.evrard@student.uclouvain.be>
2021-03-21 00:37:54 +0100

    sync helpers

--- src/helpers.py
+++ src/helpers.py
@@ -6,30 +6,30 @@ def total_rotational_increment(old_dir, new_dir):
     returns: The difference in radians between the two rotations.
     """
     if old_dir == "up":
-            if new_dir == "right":
-                return -pi/2
-            elif new_dir == "down":
-                self.radians += 2*pi/2
-            elif new_dir == "left":
-                self.radians += pi/2
-        elif old_dir == "right":
-            if new_dir == "up":
-                self.radians += pi/2
-            elif new_dir == "down":
-                self.radians -= pi/2
-            elif new_dir == "left":
-                self.radians -= 2*pi/2
-        elif old_dir == "down":
-            if new_dir == "right":
-                self.radians += pi/2
-            elif new_dir == "up":
-                self.radians -= 2*pi/2
-            elif new_dir == "left":
-                self.radians -= pi/2
-        elif old_dir == "left":
-            if new_dir == "right":
-                self.radians += 2*pi/2
-            elif new_dir == "down":
-                self.radians += pi/2
-            elif new_dir == "up":
-                self.radians -= pi/2
+        if new_dir == "right":
+            return -pi/2
+        elif new_dir == "down":
+            return 2*pi/2
+        elif new_dir == "left":
+            return pi/2
+    elif old_dir == "right":
+        if new_dir == "up":
+            return pi/2
+        elif new_dir == "down":
+            return -pi/2
+        elif new_dir == "left":
+            return -2*pi/2
+    elif old_dir == "down":
+        if new_dir == "right":
+            return pi/2
+        elif new_dir == "up":
+            return -2*pi/2
+        elif new_dir == "left":
+            return -pi/2
+    elif old_dir == "left":
+        if new_dir == "right":
+            return 2*pi/2
+        elif new_dir == "down":
+            return pi/2
+        elif new_dir == "up":
+            return -pi/2
